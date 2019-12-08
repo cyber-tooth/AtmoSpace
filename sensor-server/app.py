07528c95b509de4ec0307ba4108eb4499e19fc83
lamisabdu <lamisabdu@hotmail.com>
2019-12-08 01:05:39 +0100

    Added templates and endpoints for the landing pages, pass temp and hum to template

--- sensor-server/app.py
+++ sensor-server/app.py
@@ -20,10 +20,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import sys
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 import Adafruit_DHT
 
 app = Flask(__name__)
+
+@app.route("/lp1")
+def home():
+    return render_template("index.html")
+
+@app.route("/lp2")
+def lp2():
+    return render_template("lp2.html")
 
 @app.route("/")
 def get_sensor_data():
@@ -43,6 +51,56 @@ def get_sensor_data():
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         return jsonify({'temp': temperature, 'hum': humidity})
+    else:
+        print('Failed to get reading. Try again!')
+        sys.exit(1)
+
+@app.route("/lp2-data")
+def get_sensor_data_for_lp():
+    sensor = 11
+    pin = 4
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    # Un-comment the line below to convert the temperature to Fahrenheit.
+    # temperature = temperature * 9/5.0 + 32
+
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        return render_template("lp2.html", temperature = temperature, humidity= humidity )
+    else:
+        print('Failed to get reading. Try again!')
+        sys.exit(1)
+
+@app.route("/temperature")
+def get_sensor_temp():
+    sensor = 11
+    pin = 4
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if temperature is not None:
+
+        return jsonify({'temperature': temperature})
+    else:
+        print('Failed to get reading. Try again!')
+        sys.exit(1)
+
+@app.route("/humidity")
+def get_sensor_humidity():
+    sensor = 11
+    pin = 4
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if humidity is not None:
+
+        return jsonify({'humidity': humidity})
     else:
         print('Failed to get reading. Try again!')
         sys.exit(1)
