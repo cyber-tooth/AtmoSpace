5604f3ef0c4959e66466f3964299205f9b3fb5ad
Sabina Mollenhauer <s0563968@htw-berlin.de>
2020-01-26 00:36:06 +0100

    T15.3 Frontend aktualisiert Daten aus Datenbank automatisch, regelmäßig und es existiert ein php-skript, das die DB mit Sensordaten aktualisiert

---
+++ TW_Klima/submitdata.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Eventuell muss noch "sudo apt install php-mysql" auf dem webserver installiert werden, damit das Skript komplett ausgeführt werden.
+ * Das Skript wurde schon mit einfachen Werten getestet.
+ * Die Zahlenwerte nur zum testen. Wenn das Skript in die Tabelle schreibt, dann htmlspecialchars... entkommentieren, die Zahlenwerte löschen und mit dem PI verbinden:)
+ * Ich habe keinen Wert für die Zeit und das Datum angegeben, da meine Testtabelle eine Timestamp automatisch hinzufügt.
+*/
+$temperature = 38.5;//htmlspecialchars($_POST["temperature"]);
+$humidity = 87.3;//htmlspecialchars($_POST["humidity"]);
+$loudness = 37.7;//htmlspecialchars($_POST["loudness"]);
+
+echo "temperature: $temperature humidity: $humidity loudness: $loudness";
+
+$servername = "localhost"; //Da das skript auf dem gleichen Webserver läuft wie die DB, ist das richtig. Sonst die IP-Adresse, wo die DB läuft.
+$username = "atmospace"; //this has to be replaced with the actual username when implemented
+$password = "atmospace"; //this should also be replaced when implemented
+$dbname = "atmospace"; //and the database probably also has a different name
+
+//Create connection:
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+if($conn->connect_error) {
+    die("Connection failed; " . $conn->connect_error);
+}
+
+//insert statement to insert data into table. Here "atmospacetable must be replaced with actual table name"
+$sql = "INSERT INTO atmospacetable (temperature, humidity, loudness) VALUES ($temperature, $humidity, $loudness)";
+
+if($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn-error;
+}
+
+$conn->close();
